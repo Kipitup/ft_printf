d2f964c43bc18f9fd051ebcbc7c04c3b1cea81a1
Francis KANTE <fkante@e1r11p13.42.fr>
2019-08-26 14:41:57 +0200

    [feature/header] header file

--- header/define.h
+++ header/define.h
@@ -6,8 +6,20 @@
 /*   By: amartino <a.martino@sutdent.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 14:13:45 by amartino          #+#    #+#             */
-/*   Updated: 2019/08/26 14:13:49 by amartino         ###   ########.fr       */
+/*   Updated: 2019/08/26 14:41:21 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef DEFINE_H
+# define DEFINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+# define FALSE		0
+# define TRUE		1
+# define FAILURE	-1
+# define SUCCESS	0
+
+#endif
