e3f6af8d4229072a9308e0f22f579134c3079fa1
Francis KANTE <fkante@e1r11p13.42.fr>
2019-08-28 15:05:02 +0200

    [feature/header] define des flag et conv + struc et enum de la state machine

--- header/define.h
+++ header/define.h
@@ -6,7 +6,7 @@
 /*   By: amartino <a.martino@sutdent.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 14:13:45 by amartino          #+#    #+#             */
-/*   Updated: 2019/08/26 14:41:21 by fkante           ###   ########.fr       */
+/*   Updated: 2019/08/28 15:04:10 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,45 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h> //warning
+# include <stdint.h>
 # include <stdarg.h>
+# include "../libftprintf/libft.h"
 
 # define FALSE		0
 # define TRUE		1
 # define FAILURE	-1
 # define SUCCESS	0
+# define ON			1
+# define OFF		0
 
+# define CONVERSION_SIGN	'%'
+
+
+# define FLAG_HH		0x0000000000000001
+# define FLAG_LL		0x0000000000000002
+# define FLAG_H			0x0000000000000004
+# define FLAG_L			0x0000000000000008
+# define FLAG_L_MAJ		0x0000000000000010
+# define FLAG_PLUS		0x0000000000000020
+# define FLAG_MINUS		0x0000000000000040
+# define FLAG_HASH		0x0000000000000080
+# define FLAG_ZERO		0x0000000000000100
+# define FLAG_SPACE		0x0000000000000200
+
+# define CONV_C			0x0000000000001000
+# define CONV_S			0x0000000000002000
+# define CONV_P			0x0000000000004000
+# define CONV_D			0x0000000000008000
+# define CONV_I			0x0000000000010000
+# define CONV_O			0x0000000000020000
+# define CONV_U			0x0000000000040000
+# define CONV_X			0x0000000000080000
+# define CONV_X_MAJ		0x0000000000100000
+# define CONV_F			0x0000000000200000
+
+/*
+# define STATE_DECLARE(state_function, event_data) \
+	static void ST_STATE_FUNCT(t_state_machine *self, event_data *pEventData)
+	*/
 #endif
--- header/ft_printf.h
+++ header/ft_printf.h
@@ -6,34 +6,51 @@
 /*   By: amartino <a.martino@sutdent.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 14:14:27 by amartino          #+#    #+#             */
-/*   Updated: 2019/08/26 14:56:01 by amartino         ###   ########.fr       */
+/*   Updated: 2019/08/28 15:04:14 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define FLAG_HH			0x0000000000000001
-#define FLAG_H			0x0000000000000002
-#define FLAG_LL			0x0000000000000004
-#define FLAG_LL			0x0000000000000008
-#define FLAG_L			0x0000000000000010
-#define FLAG_L_MAJ		0x0000000000000020
+#ifndef DEFINE_H
+# define DEFINE_H
 
-#define FLAG_PLUS		0x0000000000000040
-#define FLAG_MINUS		0x0000000000000080
-#define FLAG_HASH		0x0000000000000100
-#define FLAG_ZERO		0x0000000000000200
-#define FLAG_SPACE		0x0000000000000400
+# include "define.h"
 
-#define CONV_C			0x0000000000001000
-#define CONV_S			0x0000000000002000
-#define CONV_P			0x0000000000004000
-#define CONV_D			0x0000000000008000
-#define CONV_I			0x0000000000010000
-#define CONV_O			0x0000000000020000
-#define CONV_U			0x0000000000040000
-#define CONV_X			0x0000000000080000
-#define CONV_X_MAJ		0x0000000000100000
-#define CONV_F			0x0000000000200000
+typedef enum	main_states
+{
+	ST_STRING,
+	ST_FLAG,
+	ST_CONVERSION,
+	ST_PRINT,
+	ST_ERROR
+}				e_main_states;
 
+typedef struct	s_state_machine
+{
+	void				*p_cursor;
+	unsigned long long	option;
+	e_main_states		state;
+}				t_state_machine;
+
+typedef void (*tab_statefunc)(t_state_machine *self, void *p_cursor);
+
+void	st_string (char *string);
+void	st_conversion();
+int		is_conversion();
+int		is_width();
+int		is_precision();
+int		is_cast();
+int		is_conversion_specifier();
+int		is_flag();
+
+typedef enum	type_flag
+{
+	E_TYPE_DL,
+	E_TYPE_HH,
+}				e_type_flag;
+
+#endif
+
+/*
 typedef unsigned long t_option
 
 t_option option;
@@ -48,13 +65,27 @@ if (input[i] == 'h')
 	if ((option & FLAG_H) == FALSE)
 		nb = (char)nb;
 
-
-		00000000
+{
+00000000
 		01000000
 
 		01000000
-
+		
 		00010000
 		01010000
 
 		00000000
+		}
+
+typedef struct
+{
+	char *output_str;
+}stdout_to_be_print;
+
+
+** State machine EVENT functions
+EVENT_DECLARE(ST_print, end_of_input)
+EVENT_DECLARE(ST_conversion, found_conversion_sign)
+
+** State machine STATE functions
+STATE_DECLARE(ST_STRING, */
