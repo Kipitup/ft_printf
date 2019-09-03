fff1f813aecaf705f3b4f5f097639f7f7646688a
Francis KANTE <fkante@e1r11p13.42.fr>
2019-09-03 09:52:17 +0200

    [CONVERSION] conv done for u

--- conversion/conv_to_u_decimal.c
+++ conversion/conv_to_u_decimal.c
@@ -1,9 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conv_to_u_decimal.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/03 09:47:42 by fkante            #+#    #+#             */
+/*   Updated: 2019/09/03 09:51:33 by fkante           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 
 t_vector		*conv_to_u_decimal(va_list *args_printf, uint64_t flag)
 {
-	(void)*args_printf;
+	t_vector	*vector;
+	char		*nb_itoa;
+	uint64_t	nbr;
+
 	(void)flag;
-	return (NULL);
+	nbr = va_arg(*args_printf, uint64_t);
+	if ((vector = vct_new(0)) == NULL)
+		return (NULL);
+	if((nb_itoa = ft_u_itoa_base(nbr, 10)) == NULL)
+		return (NULL);
+	if ((vct_strjoin(vector, nb_itoa)) == FAILURE)
+	{
+		vct_del(&vector);
+		return (NULL);
+	}	
+	return (vector);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fkante <fkante@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 15:19:55 by fkante            #+#    #+#             */
-/*   Updated: 2019/09/03 09:38:00 by fkante           ###   ########.fr       */
+/*   Updated: 2019/09/03 09:51:00 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ int		main(int ac, char **av)
 	//char *str;
 
 	//str = NULL;
-	
+	/*	
 	ft_printf(av[1], av[2], ac <= 3 ? NULL : av[3],
 						ac <= 4 ? NULL : av[4], ac <= 5 ? NULL : av[5]);
 	printf("------------\n");
 	printf(av[1], av[2], ac <= 3 ? NULL : av[3],
 						ac <= 4 ? NULL : av[4], ac <= 5 ? NULL : av[5]);
-	/*
+	*/
 	ft_printf(av[1], atoi(av[2]));
 	printf("------------\n");
 	printf("Leur Printf:\t");
 	printf(av[1], atoi(av[2]));
-	*/
+	
 	return (0);
 }
 
